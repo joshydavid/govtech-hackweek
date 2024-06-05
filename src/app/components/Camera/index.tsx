32993770cb12c878c38ecc2a840977d274192943
joshuadavidang <joshuadavidang@outlook.sg>
2024-06-06 07:22:20 +0800

    fix: refactor camera component to shorten code and make it cleaner
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- src/app/components/Camera/index.tsx
+++ src/app/components/Camera/index.tsx
@@ -29,53 +29,61 @@ export default function Camera() {
     setSelected(tabs[3].label);
   };
 
+  const renderCaptureAndCancelButton = () => {
+    return (
+      <>
+        <div
+          className="relative flex items-center justify-center"
+          onClick={handleCamera}
+        >
+          <div className="absolute flex h-24 w-24 items-center justify-center rounded-full">
+            <div className="h-16 w-16 rounded-full bg-white"></div>
+          </div>
+          <div className="border-1 relative z-10 h-14 w-14 rounded-full border-2 border-black bg-white"></div>
+        </div>
+        <p className="text-sm text-white" onClick={handleReset}>
+          Cancel
+        </p>
+      </>
+    );
+  };
+
+  const renderDeleteAndSubmitButton = () => {
+    return (
+      <>
+        <div className="flex justify-center">
+          <MdDelete
+            size={60}
+            onClick={() => setImage(null)}
+            className="rounded-full bg-white p-3"
+          />
+        </div>
+        <div>
+          <Button variant="white" onClick={handleSubmit} size="sm">
+            Submit
+          </Button>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="mx-auto flex max-h-screen w-screen flex-col justify-center bg-black">
-      {!image && (
+      {!image ? (
         <ReactCamera
           ref={camera}
           errorMessages={errMessages}
           facingMode="environment"
         />
-      )}
-      {image && (
-        <div>
-          <Image src={image} alt="Photo" fill={true} />
-        </div>
+      ) : (
+        <Image src={image} alt="Photo" fill={true} />
       )}
       <div className="fixed bottom-0 mx-auto grid w-full grid-cols-3 items-center justify-center bg-black p-8 text-center">
         <span></span>
-        {!image && (
-          <>
-            <div
-              className="relative flex items-center justify-center"
-              onClick={handleCamera}
-            >
-              <div className="absolute flex h-24 w-24 items-center justify-center rounded-full">
-                <div className="h-16 w-16 rounded-full bg-white"></div>
-              </div>
-              <div className="border-1 relative z-10 h-14 w-14 rounded-full border-2 border-black bg-white"></div>
-            </div>
-            <p className="text-sm text-white" onClick={handleReset}>
-              Cancel
-            </p>
-          </>
-        )}
-        {image && (
-          <>
-            <div className="flex justify-center">
-              <MdDelete
-                size={60}
-                onClick={() => setImage(null)}
-                className="rounded-full bg-white p-3"
-              />
-            </div>
-            <div>
-              <Button variant="white" onClick={handleSubmit} size="sm">
-                Submit
-              </Button>
-            </div>
-          </>
+        {!image ? (
+          <>{renderCaptureAndCancelButton()}</>
+        ) : (
+          <>{renderDeleteAndSubmitButton()}</>
         )}
       </div>
     </div>
