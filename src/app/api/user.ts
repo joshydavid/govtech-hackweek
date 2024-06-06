cf930430951d81bbebcc7c1b7567c3adac6cf028
joshuadavidang <joshuadavidang@outlook.sg>
2024-06-07 07:41:06 +0800

    fix: revalidate data bug
    Signed-off-by: joshuadavidang <joshuadavidang@outlook.sg>

--- src/app/api/user.ts
+++ src/app/api/user.ts
@@ -5,7 +5,6 @@ export const useUser = () => {
   const getUserInfo = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/${process.env.NEXT_PUBLIC_USER_INFO}`;
   const { data, error, isLoading } = useSWR(getUserInfo, backendAxiosGet, {
     revalidateOnFocus: false,
-    revalidateOnMount: false,
   });
   return { data, error, isLoading };
 };
